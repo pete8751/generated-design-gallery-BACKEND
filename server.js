fe3ab1232d92078499682ad87cffdaef2d53ff6c
pete8751 <pete8751@github.io>
2023-09-08 18:02:35 -0400

    corrected PORT bug

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors())
 //THIS VARIABLE IS ENVIRONMENTAL, IT IS INITIALIZED IN BASH.
-const PORT = process.env.port || 3000;
+const PORT = process.env.PORT || 3000;
 
 const db = knex({
     client: 'pg',
