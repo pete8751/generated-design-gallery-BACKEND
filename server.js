b8f366001ef021549e6cf7d45b21e91630804c0e
pete8751 <pete8751@github.io>
2024-01-10 10:30:05 -0500

    Enabling Cors

--- server.js
+++ server.js
@@ -50,6 +50,14 @@ db.raw('SELECT 1')
 
 //   postgresql://onlinegenerateddesigndatabase_user:6ZOo0XsuTB3nvcCAdNr8XL11a4Pi4hOS@dpg-cjtprj95mpss739vapu0-a.oregon-postgres.render.com/onlinegenerateddesigndatabase
 
+// Enable CORS for all routes
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', 'https://pete8751.github.io');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 const defaultObj = {
     Height: ['0', '100'],
     Price: ['0', '100000'],
